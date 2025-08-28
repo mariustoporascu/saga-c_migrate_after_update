e023334ac092c9dfa55fdf849139d9d40c6bfb77
mariustoporascu <mariustoporascu@outlook.com>
2025-08-28 07:53:22 +0300

    export_schema

--- export_schema.sql
+++ export_schema.sql
@@ -1,17 +1,18 @@
-SET LIST ON;
+SET LIST OFF;
+SET HEADING ON;
 SET ECHO OFF;
-SET HEADING OFF;
 SET TERM ^ ;
 
--- Output all tables and their columns
-SELECT rdb$relation_name AS table_name,
-       rdb$field_name AS column_name,
-       rdb$field_type AS field_type,
-       rdb$null_flag AS not_null,
-       rdb$default_source AS default_value
-FROM rdb$relation_fields
-WHERE rdb$system_flag = 0
-ORDER BY rdb$relation_name, rdb$field_position
+SELECT 
+    rf.rdb$relation_name AS table_name,
+    rf.rdb$field_name AS column_name,
+    f.rdb$field_type AS field_type_code,
+    rf.rdb$null_flag AS not_null,
+    rf.rdb$default_source AS default_value
+FROM rdb$relation_fields rf
+JOIN rdb$fields f ON rf.rdb$field_source = f.rdb$field_name
+WHERE rf.rdb$system_flag = 0
+ORDER BY rf.rdb$relation_name, rf.rdb$field_position
 ^
 
 QUIT
